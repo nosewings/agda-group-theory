4bea66982653f25cfd9dffc6e62ff321b1775b0c
Nicholas Coltharp <coltharp@pdx.edu>
2023-05-07 00:23:11 -0700

    Flake | Init
    - Add `.envrc`
    - Add `flake.lock`
    - Add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.default = pkgs.mkShell {
+      packages = [
+        pkgs.agda
+      ];
+    };
+  });
+}
